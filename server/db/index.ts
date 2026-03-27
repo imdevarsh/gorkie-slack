1c729b039ecdc2423149c67c769c7e8e92ac2bce
Anirudh Sriram <techwithanirudh@gmail.com>
2026-03-27 17:50:58 +0000

    chore: migrate to psql

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,14 +1,14 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
 
 import { env } from '~/env';
 /* biome-ignore lint/performance/noNamespaceImport: Drizzle schema namespace import is intentional. */
 import * as schema from './schema';
 
-const sql = neon(env.DATABASE_URL);
+const pool = new Pool({ connectionString: env.DATABASE_URL });
 
 export const db = drizzle({
-  client: sql,
+  client: pool,
   schema,
   casing: 'snake_case',
 });
